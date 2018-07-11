815dcb2931673da22db2e77bd12f5c324de1775b
Adonis Bou Chakra <abo@hinderlingvolkart.com>
2018-07-11 17:23:56 +0200

    [abo] update

---
+++ build/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Boilerplate for Gulp">
+		<meta name="author" content="Adonis Bou Chakra">
+		<title>Alfred Müller - Brand Refresh</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!--[if lt IE 9]-->
+		<script src="js/vendor/modernizr-2.8.3.min.js"></script>
+		<!--[endif]-->
+	</head>
+	<body>
+		<div class="layers">
+			<div class="layers--item"></div>
+			<div class="layers--item"></div>
+			<div class="layers--item"></div>
+		</div>
+		<div class="layers_bg"></div>
+		<script data-main="js/ui.js" src="js/vendor/require.js"></script>
+	</body>
+</html>
---
+++ build/pug/components/layers/layers.php
@@ -0,0 +1,7 @@
+
+<div class="layers">
+	<div class="layers--item"></div>
+	<div class="layers--item"></div>
+	<div class="layers--item"></div>
+</div>
+<div class="layers_bg"></div>
